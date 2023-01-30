514a3d3961e5ed7f3b997edd5314649a53abe7a8
nazanin jaberzadeh <n.jaberz2001@gmail.com>
2023-01-30 11:53:38 +0330

    11

---
+++ t_1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace program
+{
+    class test
+    {
+        public static void Main(string[] args)
+        {
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            int a2 = Convert.ToInt32(Console.ReadLine());
+            int a3 = Convert.ToInt32(Console.ReadLine());
+
+            if (a1 + a2 > a3 && a1 + a3 > a2 & a2 + a3 > a1)
+            {
+                Console.WriteLine(M(a1, a2, a3));
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+        private static int M(int x, int y, int z)
+        {
+            int m;
+            m = x + y + z;
+            return m;   
+            
+        }
+    }
+}
